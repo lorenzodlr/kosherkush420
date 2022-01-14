68677385a6792803db7dbb442a5479f08b9d8535
roadarloceo <82858292+roadarloceo@users.noreply.github.com>
2022-01-13 19:20:24 -0800

    com

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,12 @@ function App() {
           <h3>Kosher Kush 420</h3>
         </div>
         <div className='links-container'>
-          <a>PooCoin Chart</a>
-          <a>Twitter</a>
-          <a>Reddit</a>
-          <a>Liquidity Lock</a>
+          <a href="https://poocoin.app/tokens/0x678d86517699ea514952b97ad750c3180fb07977" target="__blank">PooCoin Chart</a>
+          <a href="https://twitter.com/search?q=%23kosherkush420&src=typed_query" target="__blank">Twitter</a>
+          <a href="https://deeplock.io/lock/0x1c6657cae128C664a563Bc461eE1bd1108Ac2Efa" target="__blank">Liquidity Lock</a>
         </div>
         <div className='contract-container'>
-          <h1>contract address</h1>
+          <h1>0x678d86517699ea514952b97ad750c3180fb07977</h1>
           <h5>Getting rich like the Jews</h5>
         </div>
         <div className='extras'>
